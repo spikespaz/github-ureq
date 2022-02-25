fafbdbb2af4d58aacd675d504b37e045644d7dd0
Jacob Birkett <jacob@birkett.dev>
2022-02-24 21:07:57 -0700

    Move strong response types to submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-pub mod license;
-pub mod release;
-pub mod repository;
-pub mod user;
+pub mod types;
 
-pub use license::*;
-pub use release::*;
-pub use repository::*;
-pub use user::*;
+pub use types::*;
---
+++ src/types/mod.rs
@@ -0,0 +1,9 @@
+pub mod license;
+pub mod release;
+pub mod repository;
+pub mod user;
+
+pub use license::*;
+pub use release::*;
+pub use repository::*;
+pub use user::*;
